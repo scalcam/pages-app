02e109852bc9b9c9d6c1c44a33f5276197631df5
Spencer Calcamuggio <scalcamuggio77@gmail.com>
2019-08-10 11:37:30 -0400

    add page routing between home and test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TestComponent } from './components/test/test.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', pathMatch: 'full', component: HomeComponent },
+  { path: 'test', pathMatch: 'full', component: TestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
