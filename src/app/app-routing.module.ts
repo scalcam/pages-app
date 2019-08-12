1b63fbfbacc49789f06b929f1ef2f4a791089dd1
Spencer Calcamuggio <scalcamuggio77@gmail.com>
2019-08-12 18:56:20 -0400

    add people page to nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
+import { PeopleComponent } from './components/people/people.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', pathMatch: 'full', component: HomeComponent },
-  { path: 'test', pathMatch: 'full', component: TestComponent }
+  { path: 'test', pathMatch: 'full', component: TestComponent },
+  { path: 'people', pathMatch: 'full', component: PeopleComponent }
 ];
 
 @NgModule({
