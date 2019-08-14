f883bc19c1af7d7410f95247c483e9b2d9ea7645
Spencer Calcamuggio <scalcamuggio77@gmail.com>
2019-08-13 21:26:30 -0400

    add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
 import { PeopleComponent } from './components/people/people.component';
+import { AddPersonComponent } from './components/add-person/add-person.component';
+import { EditPersonComponent } from './components/edit-person/edit-person.component';
+import { PersonListComponent } from './components/person-list/person-list.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', pathMatch: 'full', component: HomeComponent },
   { path: 'test', pathMatch: 'full', component: TestComponent },
-  { path: 'people', pathMatch: 'full', component: PeopleComponent }
+  { path: 'people', pathMatch: 'full', component: PeopleComponent },
+  { path: 'add-person', pathMatch: 'full', component: AddPersonComponent },
+  { path: 'edit-person/:id', pathMatch: 'full', component: EditPersonComponent },
+  { path: 'person-list', pathMatch: 'full', component: PersonListComponent }
 ];
 
 @NgModule({
