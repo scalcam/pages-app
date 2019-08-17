2b36d7106ddb640bfce960ad482c564f1696aff0
Spencer Calcamuggio <scalcamuggio77@gmail.com>
2019-08-16 22:26:57 -0400

    add routing to firebase page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
+import { FirebaseTestingComponent } from './components/firebase-testing/firebase-testing.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', pathMatch: 'full', component: HomeComponent },
-  { path: 'test', pathMatch: 'full', component: TestComponent }
+  { path: 'test', pathMatch: 'full', component: TestComponent },
+  { path: 'firebase', pathMatch: 'full', component: FirebaseTestingComponent }
 ];
 
 @NgModule({
