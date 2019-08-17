7deaaa9e4ad12c7b6e81927ee3b6d9fe4a0ac8c0
Spencer Calcamuggio <scalcamuggio77@gmail.com>
2019-08-16 22:49:09 -0400

    update app module for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabase } from '@angular/fire/database';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,7 @@ import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FirebaseTestingComponent } from './components/firebase-testing/firebase-testing.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,10 @@ import { FirebaseTestingComponent } from './components/firebase-testing/firebase
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
